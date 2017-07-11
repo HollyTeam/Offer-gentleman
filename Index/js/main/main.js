748adb6e7ae0d5d49df58abb4a9b028c3980a23c
regaliastar <1183080130@qq.com>
2017-07-11 22:58:08 +0800

    main.js v1.1.0

--- Index/js/main/main.js
+++ Index/js/main/main.js
@@ -3,6 +3,7 @@
 /**
  * 用于服务器的上传操作
  * tips: 未测试
+ * @param {function} Resume.prototype.setResume -- 用于得到整张表单数据
  */
 (function(){
     var currentInstance = null;
@@ -18,18 +19,14 @@
             if(Resume.instance){
                 throw new Error('不能重复实例化类！');
             }
-            this.id = null,
             this.name = null,
-            this.photo = null,
             this.address = null,
             this.phone = null,
             this.mail = null,
-            this.language = null,
-            this.skill = null,
-            this.hobby = null,
-            this.related_info = null,
-            this.edus = [],
-            this.works = []
+            this.edus = {},
+            this.works = [],
+            this.leader = {},
+            this.skill = {},
         }
 
         /**
@@ -78,63 +75,77 @@
      */
     Resume.prototype.setResume = function(){
         var _self = this;
-        this.id = $('input.resume-id').val();
 
-        this.name = $('div.name').html();   //pass
+        this.name = $('div.name').html();
 
-        this.photo = $('div.photo').html();
+        this.address = $('div.address').html();
 
-        this.address = $('div.address').html(); //pass
+        this.phone = $('div.phone').html();
 
-        this.mail = $('div.mail').html();   //pass
+        this.mail = $('div.mail').html();
 
-        this.language = $('div.language').html();   //pass
+        this.skill.language = $('div.language').html();
 
-        this.skill = $('div.skill').html(); //pass
+        this.skill.computer = $('div.computer').html();
 
-        this.hobby = $('div.hobby').html(); //pass
-
-        this.related_info = $('div.related-info').html();   //pass
+        this.skill.hobby = $('div.hobby').html();
 
         //pass
-        $('.parent-edu .add-blank').each(function(edu) {
-            var _edu = {};
-            _edu.id = $(this).find('input.edu-exp-id').val();
-            _edu.school = $(this).find('div.school').html();
-            _edu.education = $(this).find('div.education').html();
-            _edu.subject = $(this).find('div.subject').html();
-            _edu.edu_experience = $(this).find('div.edu-experience').html();
-            _edu.city = $(this).find('div.city').html();
-            _edu.start_time = $(this).find('div.start-time').html();
-            _edu.end_time = $(this).find('div.end-time').html();
-            _self.edus.push(_edu);
-        });
-        //alert(JSON.stringify(this.edus[0]));
+        this.edus.school = $('.edus').find('div.school').html();
+        this.edus.city = $('.edus').find('div.city').html();
+        this.edus.province = $('.edus').find('div.province').html();
+        this.edus.college = $('.edus').find('div.college').html();
+        this.edus.end_time = $('.edus').find('div.end-time').html();
+        this.edus.grade = $('.edus').find('div.grade').html();
+        this.edus.honors = $('.edus').find('div.honors').html();
+        this.edus.related_course = $('.edus').find('div.related_course').html();
+        //alert(JSON.stringify(this.edus));
 
         //pass
-        var spe = '|';
-        $('.parent-work, .parent-practice').children('.add-blank').each(function(work) {
+        var spe = '#|#';
 
+        $('.parent-works').children('.add-blank').each(function(work) {
             if($(this).parent().hasClass('deleted')) {
                 return;
             }
             var _work = {};
-            _work.id = $(this).find('input.work-exp-id').val();
             _work.company = $(this).find('div.company').html();
-            _work.company_des = $(this).find('div.company_des').html();
-            _work.department = $(this).find('div.department').html();
-            _work.position = $(this).find('div.position').html();
-            $(this).find('div.experience-desc').each(function() {
-                _work.experience_desc += spe + $(this).html().trim();
-            });
-            _work.experience_desc = _work.experience_desc.substring(spe.length);
             _work.city = $(this).find('div.city').html();
+            _work.province = $(this).find('div.province').html();
+            _work.position = $(this).find('div.position').html();
+            _work.project = $(this).find('div.project').html();
             _work.start_time = $(this).find('div.start-time').html();
             _work.end_time = $(this).find('div.end-time').html();
-            _work.type = $(this).parent().attr('data-type');
+            _work.sentence_1 = $(this).find('div.sentence_1').html();
+            _work.sentence_2 = $(this).find('div.sentence_1').html();
+            _work.sentence_3 = $(this).find('div.sentence_1').html();
+            _work.sentence_4 = $(this).find('div.sentence_1').html();
             _self.works.push(_work);
         });
         //alert(JSON.stringify(this.works[0]));
+
+        $('.leader').find('div.organization').children().each(function(org){
+            var _organization = {};
+            var _organization.name = $(this).find('div.name').html();
+            var _organization.position = $(this).find('div.position').html();
+            var _organization.start_time = $(this).find('div.start_time').html();
+            var _organization.end_time = $(this).find('div.end_time').html();
+            var _organization.sentence_1 = $(this).find('div.sentence_1').html();
+            var _organization.sentence_2 = $(this).find('div.sentence_2').html();
+            var _organization.sentence_3 = $(this).find('div.sentence_3').html();
+            _self.leader.organization.push(_organization);
+        })
+
+        $('.leader').find('div.club').children().each(function(clu){
+            var _club = {};
+            var _club.name = $(this).find('div.name').html();
+            var _club.position = $(this).find('div.position').val();
+            var _club.start_time = $(this).find('div.start_time').val();
+            var _club.end_time = $(this).find('div.end_time').val();
+            var _club.sentence_1 = $(this).find('div.sentence_1').val();
+            var _club.sentence_2 = $(this).find('div.sentence_2').val();
+            _self.leader.club.push(_club);
+        })
 
     }
 
@@ -144,7 +155,7 @@
     Resume.prototype.saveResume = function(INSTANCE){
         var date = INSTANCE;
         $.ajax({
-            url:'/main/save',
+            url:'/main',
             type:'post',
             date:date,
             success:function(){
@@ -182,7 +193,7 @@
    }
 
    //若浏览器不支持，则给出提示
-   //cs();
+   cs();
    $('#waring_close').on("click",function () {
        $('#waring').hide();
 
